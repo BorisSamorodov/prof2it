681b163f1b19bb3c7ce12a51ad29f6ad66a4d975
BorisSamorodov <b.samorodov.karazin@gmail.com>
2024-11-01 15:46:28 +0200

    Add files via upload

---
+++ BorisBot/Controllers/BotActions/Journals/JournalCreate.cs
@@ -0,0 +1,20 @@
+﻿using BorisBot.DTO;
+
+namespace BorisBot.BotActions.Journals;
+
+public class JournalCreate : BaseBotAction
+{
+    public JournalCreate(ChatDetails details) : base(details)
+    {
+    }
+
+    public override async Task Perform()
+    {
+        UserState = new UserStateJson
+        {
+            IsCreatingJournal = true
+        };
+        
+        await Say("Введіть назву нового журналу", false);
+    }
+}
---
+++ BorisBot/Controllers/BotActions/Journals/JournalGet.cs
@@ -0,0 +1,44 @@
+﻿using BorisBot.DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace BorisBot.BotActions.Journals;
+
+public class JournalGet : ParameterBotAction
+{
+    public JournalGet(ChatDetails details, string[] parameters) : base(details, parameters)
+    {
+    }
+
+    public override async Task Perform()
+    {
+        var journalId = Guid.Parse(Parameters.First());
+        var menu = await GetSingleJournalMenu(journalId);
+        await ShowButtons(menu);
+    }
+    
+    private async Task<TelegramMenu> GetSingleJournalMenu(Guid journalId)
+    {
+        var issues = await Details.Database.JournalIssues
+            .Where(x => x.ScientificJournal.Id == journalId)
+            .OrderBy( x => x.Date)
+            .ToArrayAsync();
+        
+        var markup = new TelegramReplyMarkup();
+        foreach (var i in issues)
+        {
+            markup.AddButtons(i.Date.ToShortDateString(), "issues.get/" + i.Id);
+        }
+        
+        markup.AddButtons("Додати випуск", "issues.create/" + journalId);
+        
+        var menu = new TelegramMenu
+        {
+            ChatId = Details.ChatId,
+            Text = "Випусків журналів",
+            Markup = markup
+        };
+
+        return menu;
+        
+    }
+}
